5a1d880b118f7192e17c7fe4196c84418cae8284
moha4733 <moonyahidi@gmail.com>
2026-04-04 21:56:51 +0200

    added change

---
+++ src/test/java/com/example/finance_tracker/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.finance_tracker;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
